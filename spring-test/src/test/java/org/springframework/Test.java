ff47c22db6fc3ff84a1aee71e92edc186ae572a6
zhouxin <867833821@qq.com>
2020-02-14 19:49:02 +0800

    Initial commit

---
+++ spring-test/src/test/java/org/springframework/Test.java
@@ -0,0 +1,12 @@
+package org.springframework;
+
+public class Test {
+	@org.junit.Test
+	public void test1() {
+		System.out.println("zx");
+	}
+
+	public static void main(String[] args) {
+		System.out.println("zz");
+	}
+}
